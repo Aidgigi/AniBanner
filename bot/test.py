79df4cf840b40d59bc3fc85d9956c7dbf991eec8
Aidan Ginise <aidgigi357@gmail.com>
2021-06-18 12:29:21 -0700

    bot stuff

---
+++ bot/test.py
@@ -0,0 +1,100 @@
+from discord.ext.commands import Bot
+import discord
+import uuid, requests, base64, os, random
+import numpy as np
+
+from tensorflow.keras.preprocessing import image
+
+import tensorflow as tf
+
+from PIL import Image
+
+# preparing the tf model
+model = tf.keras.models.load_model("./trained_model")
+
+client = Bot(command_prefix = ["!"])
+
+allowed_channels = (854627368643592207, 855306527484542976, 855309516580061207)
+
+def auto_uuid():
+    return str(base64.urlsafe_b64encode(uuid.uuid1().bytes)).replace('=', '')[:-1:][2::]
+
+def fetch_gradient(url):
+    img = requests.get(url).content
+
+    fname = f"{auto_uuid()}.jpg"
+
+    with open(fname, 'wb') as f:
+        f.write(img)
+
+    img = image.load_img(fname, target_size = (224, 224))
+    img = image.img_to_array(img)
+    img = np.expand_dims(img, axis = 0)
+
+    os.remove(fname)
+
+    return model.predict(img, batch_size = 10)[0][0]
+
+def gif_gradients(url):
+    img = requests.get(url).content
+
+    fname = f"{auto_uuid()}.gif"
+
+    with open(fname, 'wb') as f:
+        f.write(img)
+
+    img = Image.open(fname)
+
+    os.remove(fname)
+
+    start = 0
+    end = img.n_frames - 1
+    rand = random.randrange(start, end)
+
+    grads = []
+
+    for selector in (start, end, rand):
+        img.seek(selector)
+        img2 = img.convert('RGB')
+        fname = f"{auto_uuid()}.jpg"
+        img2.save(fname)
+
+        img2 = image.load_img(fname, target_size = (224, 224))
+        img2 = image.img_to_array(img2)
+        img2 = np.expand_dims(img2, axis = 0)
+
+        os.remove(fname)
+
+        grads.append(model.predict(img2, batch_size = 10)[0][0])
+
+    return max(grads)
+
+@client.event
+async def on_ready():
+    await client.change_presence(activity = discord.Game(name = "Aidgigi's Bitch Simulator 2021"))
+
+@client.event
+async def on_message(ctx):
+    if (atts := ctx.attachments) and ctx.channel.id in allowed_channels:
+        print(atts)
+        for att in atts:
+            if (ending := (url := att.url).lower().split('.')[-1]) not in ['jpg', 'jpeg', 'png']:
+                if ending == 'gif':
+                    grad = gif_gradients(url)
+
+            grad = fetch_gradient(url)
+            message = "Anime" if grad > 4.5 else "Not anime"
+
+            await ctx.reply(message + f"\n\n{grad}", mention_author = False)
+
+
+@client.event
+async def on_raw_reaction_add(ctx):
+    if (chan := ctx.channel_id) in allowed_channels:
+        await client.get_channel(chan).send("reaction found")
+
+
+with open("token", 'r') as f:
+    TOKEN = f.read()
+
+client.run(TOKEN)
--- training/train.py
+++ training/train.py
@@ -7,8 +7,6 @@ from tensorflow.keras.models import Sequential
 from tensorflow.python.compiler.mlcompute import mlcompute
 from tensorflow.python.training.tracking import base
 
-mlcompute.set_mlc_device(device_name = "gpu")
-
 image_size = (224, 224)
 b_size = 32
 
@@ -32,10 +30,10 @@ vds = tf.keras.preprocessing.image_dataset_from_directory(
     batch_size = b_size
 )
 
-data_augmentation = tf.keras.Sequential([
+"""data_augmentation = tf.keras.Sequential([
   tf.keras.layers.experimental.preprocessing.RandomFlip('horizontal'),
   tf.keras.layers.experimental.preprocessing.RandomRotation(0.2),
-])
+])"""
 
 
 preprocess_input = tf.keras.applications.mobilenet_v2.preprocess_input
@@ -51,7 +49,13 @@ base_model = tf.keras.applications.MobileNetV2(
 image_batch, label_batch = next(iter(tds))
 feature_batch = base_model(image_batch)
 
-base_model.trainable = False
+base_model.trainable = True
+
+fine_tune_at = 100
+
+for layer in base_model.layers[:fine_tune_at]:
+  layer.trainable =  False
+
 
 global_average_layer = tf.keras.layers.GlobalAveragePooling2D()
 feature_batch_average = global_average_layer(feature_batch)
@@ -61,8 +65,8 @@ prediction_batch = prediction_layer(feature_batch_average)
 
 
 inputs = tf.keras.Input(shape = (224, 224, 3))
-x = data_augmentation(inputs)
-x = preprocess_input(x)
+#x = data_augmentation(inputs)
+x = preprocess_input(inputs)
 x = base_model(x, training = False)
 x = global_average_layer(x)
 x = tf.keras.layers.Dropout(0.2)(x)
@@ -72,16 +76,18 @@ model = tf.keras.Model(inputs, outputs)
 base_learning_rate = 0.0001
 
 model.compile(
-    optimizer = tf.keras.optimizers.Adam(lr=base_learning_rate),
+    optimizer = tf.keras.optimizers.Adam(lr = base_learning_rate / 10),
     loss = tf.keras.losses.BinaryCrossentropy(from_logits=True),
-    metrics = ['accuracy']
+    metrics = ['accuracy'],
+    run_eagerly = False
 )
 
 epochs = 10
+fine_tune = 0 
 
 history = model.fit(
     tds,
-    epochs = epochs,
+    epochs = epochs + fine_tune,
     validation_data = vds
 )
 
