dce6609560ad1018405239f94b2bb09d65a5a2c8
Aidan Ginise <aidgigi357@gmail.com>
2021-06-18 21:41:24 -0700

    making the downloader also normalize

--- training/neg/downloader.py
+++ training/neg/downloader.py
@@ -1,9 +1,11 @@
 import requests
 from concurrent.futures import ThreadPoolExecutor
 import os
-from tqdm import tqdm 
+from tqdm import tqdm
+from PIL import Image
 
 def get_and_write(url):
+    print(f"Now Processing: {url[1]}")
     res = requests.get(url[0])
 
     if url[0].lower().endswith(".jpeg"):
@@ -11,9 +13,29 @@ def get_and_write(url):
     
     else:
         url_ending = url[0].split('.')[-1].lower()
+    
+    fname = f"images/{url[1]}.{url_ending}"
 
-    with open(f"images/{url[1]}.{url_ending}", 'wb') as f:
+    with open(fname, 'wb') as f:
         f.write(res.content)
+
+    try:
+        img = Image.open(fname)
+
+        if img.mode.lower() != 'rgb':
+            print(f"Converted: {url[1]}")
+            img = img.convert('RGB')
+
+        img = img.resize((224, 224), Image.ANTIALIAS)
+
+        os.remove(fname)
+    
+        img.save(fname.split('.')[0] + '.jpg', 'JPEG', quality = 100)
+
+    except Exception as e:
+        print(Exception)
+
+        os.remove(fname)
 
     with open("negatives_downloaded", 'a') as f:
         f.write(f"{url[0]}\n")
@@ -28,5 +50,5 @@ url_pack = []
 for url in urls:
     url_pack.append([url, len(url_pack) + 1])
 
-with ThreadPoolExecutor(max_workers = 50) as pool:
-    tqdm(pool.map(get_and_write, url_pack), total = len(url_pack))
+with ThreadPoolExecutor(max_workers = 100) as pool:
+    tqdm(list(pool.map(get_and_write, url_pack), total = len(url_pack)))
