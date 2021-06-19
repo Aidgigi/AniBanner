eaa884ab86b375fb11437ca81b7889af6d9fd8cd
Aidan Ginise <aidgigi357@gmail.com>
2021-06-18 18:45:00 -0700

    small updoot

--- training/negative_fetch.py
+++ training/negative_fetch.py
@@ -21,22 +21,21 @@ async def on_ready():
 @client.event
 async def on_message(ctx):
     global ran, attachments
-    print("1")
     if ctx.author.name == "Aidgigi" and ctx.channel.name == "off-topic" and not ran:
         ran = True
         num_found = 0
 
         async for message in ctx.channel.history(limit = None):
-            if num_found >= arg_pack[1]:
+            if num_found >= int(arg_pack[1]):
                 break
 
             if (atts := message.attachments):
-                print(len(attachments))
                 for att in atts:
                     num_found += 1
                     attachments.append(att.url)
                 
             if len(attachments) % 50 == 0:
+                print(num_found)
                 with open(arg_pack[1], "a") as f:
                     for att in attachments:
                         f.write(f"{att}\n")
