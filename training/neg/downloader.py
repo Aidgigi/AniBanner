3fa4c09b4c0be0c098b2779a19b89f74e794842c
Aidan Ginise <aidgigi357@gmail.com>
2021-06-18 22:42:23 -0700

    LOL

--- training/neg/downloader.py
+++ training/neg/downloader.py
@@ -27,7 +27,7 @@ def get_and_write(url):
 
         img = img.resize((224, 224), Image.ANTIALIAS)
     
-        img.save(f"{url[1]}.jpg", 'JPEG', quality = 100)
+        img.save(f"images/{url[1]}.jpg", 'JPEG', quality = 100)
 
     except Exception as e:
         print(e)
